f82390042804a4cca50d51df2034fe604ddd72be
Malavika Rajeev <mrajeev08@gmail.com>
2020-03-07 13:03:37 -0500

    init commit

---
+++ rsync_ex.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+## -r recurse through sub-directories
+## -L transform symlink into reference file/dir
+## -v be verbose about printing messages
+## -z compresses data before transfer and decompresses after transfer
+## -t pass the timestamp when syncing
+
+echo 'test sync'
